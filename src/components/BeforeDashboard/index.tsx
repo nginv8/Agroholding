bac9727e94e52236fdd64a62e0a9c9cad26dd199
Oleksii Oseledko <alexeyoseledko@gmail.com>
2025-09-16 16:55:34 +0100

    update dashboard wellcome component

--- src/components/BeforeDashboard/index.tsx
+++ src/components/BeforeDashboard/index.tsx
@@ -1,8 +1,7 @@
+/* eslint-disable tailwindcss/no-custom-classname */
 import { Banner } from '@payloadcms/ui/elements/Banner';
 
 import React from 'react';
-
-import { SeedButton } from './SeedButton';
 
 import './index.scss';
 
@@ -11,68 +10,78 @@ const baseClass = 'before-dashboard';
 const BeforeDashboard: React.FC = () => {
   return (
     <div className={baseClass}>
-      {}
-      {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
-      <Banner className={`${baseClass}__banner`} type="success">
-        <h4>Welcome to your dashboard! </h4>
+      <Banner type="success">
+        <h4>Payload CMS User Guide</h4>
       </Banner>
-      Here&apos;s what to do next:
-      {}
-      {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
-      <ul className={`${baseClass}__instructions`}>
-        <li>
-          <SeedButton />
-          {' with a few pages, posts, and projects to jump-start your new site, then '}
-          <a href="/" target="_blank">
-            visit your website
-          </a>
-          {' to see the results.'}
-        </li>
-        <li>
-          If you created this repo using Payload Cloud, head over to GitHub and clone it to your
-          local machine. It will be under the <i>GitHub Scope</i> that you selected when creating
-          this project.
-        </li>
-        <li>
-          {'Modify your '}
-          <a
-            href="https://payloadcms.com/docs/configuration/collections"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            collections
-          </a>
-          {' and add more '}
-          <a
-            href="https://payloadcms.com/docs/fields/overview"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            fields
-          </a>
-          {' as needed. If you are new to Payload, we also recommend you check out the '}
-          <a
-            href="https://payloadcms.com/docs/getting-started/what-is-payload"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Getting Started
-          </a>
-          {' docs.'}
-        </li>
-        <li>
-          Commit and push your changes to the repository to trigger a redeployment of your project.
-        </li>
-      </ul>
-      {'Pro Tip: This block is a '}
-      <a
-        href="https://payloadcms.com/docs/admin/custom-components/overview#base-component-overrides"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        custom component
-      </a>
-      , you can remove it at any time by updating your <strong>payload.config</strong>.
+
+      <div className={`${baseClass}__content`}>
+        <div>
+          <section>
+            <h5>What are Collections?</h5>
+            <p>
+              Collections are the main data type in Payload. They contain documents with fields that
+              you define. For example: Posts, Products, Users, Pages etc.
+            </p>
+            <ul>
+              <li>
+                <strong>Posts</strong> - for blog posts
+              </li>
+              <li>
+                <strong>Products</strong> - for products
+              </li>
+              <li>
+                <strong>Pages</strong> - for static pages
+              </li>
+              <li>
+                <strong>Media</strong> - for files and images
+              </li>
+            </ul>
+          </section>
+        </div>
+
+        <div>
+          <section>
+            <h5>What are Globals?</h5>
+            <p>
+              Globals are single documents for general site settings that are used throughout the
+              project.
+            </p>
+            <ul>
+              <li>
+                <strong>Header</strong> - header settings
+              </li>
+              <li>
+                <strong>Footer</strong> - footer settings
+              </li>
+              <li>
+                <strong>Contact Info</strong> - contact information
+              </li>
+              <li>
+                <strong>Product Page Settings</strong> - product page settings
+              </li>
+              <li>
+                <strong>Post Page Settings</strong> - post page settings
+              </li>
+            </ul>
+          </section>
+        </div>
+      </div>
+
+      <section>
+        <h5>Useful links:</h5>
+        <ul>
+          <li>
+            <a href="https://payloadcms.com/docs" target="_blank" rel="noopener noreferrer">
+              Official Payload Documentation
+            </a>
+          </li>
+          <li>
+            <a href="/" target="_blank">
+              View Site
+            </a>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 };
